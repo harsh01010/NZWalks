7fceefa35bfd21fc7977ba23366db41fd4fa5d6d
Atul <atulharsh274@gmail.com>
2024-07-17 21:46:40 +0530

    Init

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = new List<string>() { "student 1", "student 2", "student 3", "student 4" };
+            return Ok(students);
+        }
+    }
+}
